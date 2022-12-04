d6cae080d344c140c39b6f112f88c3842459fdd9
Claudio Bianco <91281998+claudio-bianco@users.noreply.github.com>
2022-12-04 17:52:17 -0300

    Update create-stack.sh

--- create-stack.sh
+++ create-stack.sh
@@ -7,7 +7,3 @@ aws cloudformation create-change-set \
 --parameters file://$4 \
 --region us-east-1 \
 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
-
-aws cloudformation execute-change-set \
---stack-name $1 \
---change-set-name $2
